100c67190cae3339f3ee632989c7d7de6d452741
tjfruddnjs1 <tjfruddnjs12@naver.com>
2021-01-12 16:29:36 +0900

    -4. multer 패키지로 이미지 업로드 구현하기

--- BackEnd/9. 익스프레스로 SNS 서비스 만들기/routes/page.js
+++ BackEnd/9. 익스프레스로 SNS 서비스 만들기/routes/page.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
@@ -19,12 +20,23 @@ router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-  const twits = [];
-  res.render('main', {
-    title: 'NodeBird',
-    twits,
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
